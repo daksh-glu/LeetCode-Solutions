3d46891dddbb55ec34572b8e03fcd48c77828199
Daksh Jain <147020461+daksh-glu@users.noreply.github.com>
2025-12-26 22:29:20 +0530

    Time: 0 ms (100%), Space: 9.8 MB (94.72%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int total = 1 << n;
+
+        for (int mask = 0; mask < total; mask++) {
+            vector<int> temp;
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    temp.push_back(nums[i]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
