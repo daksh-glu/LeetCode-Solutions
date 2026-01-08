60c137296ec1d857cbe04dfac6808405c2e4bb43
Daksh Jain <147020461+daksh-glu@users.noreply.github.com>
2026-01-08 22:54:24 +0530

    Time: 0 ms (100%), Space: 24.6 MB (27.39%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while (true) {
+            while (curr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            k--;
+            if (k == 0) return curr->val;
+            curr = curr->right;
+        }
+    }
+};
